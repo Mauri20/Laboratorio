020e62aa80af2bc47b865cbdcd98c45b8fe08e0d
Mauri20 <69651999+Mauri20@users.noreply.github.com>
2020-10-25 14:48:00 -0600

    Creacion del formulario de Cargos
    Se creó el formulario y se cargaron los datos en la tabla desde la base de datos

--- Laboratorio2/src/main/java/com/unab/edu/DAO/Clscuentasusuario.java
+++ Laboratorio2/src/main/java/com/unab/edu/DAO/Clscuentasusuario.java
@@ -5,11 +5,41 @@
  */
 package com.unab.edu.DAO;
 
+import com.unab.edu.Entidades.cuentasusuario;
+import com.unab.edu.conexionmysql.Conexion;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Stanly
  */
 public class Clscuentasusuario {
+   Conexion cn = new Conexion();
+   Connection con = cn.RetornoConexion();
    
-    
+   public ArrayList<cuentasusuario> Transacciones() {
+        var listado = new ArrayList<cuentasusuario>();
+        try {
+            CallableStatement statement = con.prepareCall("SELECT * FROM appbanco.cuentasusuario;");
+            ResultSet res = statement.executeQuery();
+            while (res.next()) {
+                cuentasusuario transaccion = new cuentasusuario();
+                transaccion.setIdcuentasusuario(res.getInt("idcuentasusuario"));
+                transaccion.setSaldo(res.getDouble("saldo"));
+                transaccion.setIdUsuario(res.getInt("idUsuario"));
+                transaccion.setTransaccion(res.getInt("transaccion"));
+                transaccion.setFecha(res.getDate("fecha"));
+                //Agregando a la lista
+                listado.add(transaccion);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "No se cargaron las Transacciones " + e);
+        }
+
+        return listado;
+    }
 }
