ec189dbef8a9aa764fb23c357ef21940f4a1408f
Elmer-Cardoza <64285134+Elmer-Cardoza@users.noreply.github.com>
2020-10-25 19:52:19 -0600

    Create conexion.java

---
+++ Laboratorio2/src/main/java/com/unab/edu/conexion/conexion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Elmer Cardoza
+ */
+public class conexion {
+
+    private Connection conexionBD;
+
+    public conexion() {
+        try {
+            conexionBD=DriverManager.getConnection("jdbc:mysql://localhost/appbanco?useTimezone=true&serverTimezone=UTC","root","root");
+            System.out.println("Conexion Existosa!");
+        } catch (Exception e) {
+            System.out.println("No se pudo realizar la Conexion "+e);
+        }
+    }
+    public Connection retornarConexion(){
+        return conexionBD;
+    }
+}
